a7ba553097a730f678effa66dbdbc37b6cc0beaf
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-13 21:10:04 +0700

    make health and damage of projectile

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Range(0f,10f)][SerializeField] float speed = 1f;
+    [SerializeField] float damage = 80f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }
