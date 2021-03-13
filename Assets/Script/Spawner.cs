822d6cf28aa515c7cac718c01607d3d1d2e0bd89
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-13 16:11:34 +0700

    make spawner

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefabs;
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefabs, transform.position, transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
