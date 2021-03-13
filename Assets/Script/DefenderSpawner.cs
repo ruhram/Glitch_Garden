78bb5c5011fc328c5028c2328514767ba6f282bb
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-13 21:58:48 +0700

    spawn at mouse position

--- Assets/Script/DefenderSpawner.cs
+++ Assets/Script/DefenderSpawner.cs
@@ -7,12 +7,19 @@ public class DefenderSpawner : MonoBehaviour
     [SerializeField] GameObject defender;
     private void OnMouseDown()
     {
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
         Debug.Log("Mouse was Clicked");
     }
 
-    private void SpawnDefender()
+    private Vector2 GetSquareClicked()
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    private void SpawnDefender(Vector2 worldPos)
+    {
+        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
     }
 }
