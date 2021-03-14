e08d8544f14f613bf9898cfe38dd49894d534713
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-14 19:48:39 +0700

    make state for attacker to attacking

--- Assets/Script/Attacker.cs
+++ Assets/Script/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)]
     float currentSpeed = 1f;
-    GameObject curremtTarget;
+    GameObject currentTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,15 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
@@ -27,6 +37,16 @@ public class Attacker : MonoBehaviour
     public void Attack(GameObject target)
     {
         GetComponent<Animator>().SetBool("isAttacking", true);
-        curremtTarget = target;
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
