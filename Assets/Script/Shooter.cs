edb72280f1168dccf0e0c4e36c63dd39acb0a79b
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-14 13:26:38 +0700

    attack in line

--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -5,7 +5,48 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
-    
+    Spawner myLaneSpawner;
+
+    private void Start()
+    {
+        setLaneSpawner();
+    }
+    public void Update()
+    {
+        if (isAttackerInLine())
+        {
+            Debug.Log("shoot");
+        }
+        else
+        {
+            Debug.Log("Sit and wait");
+        }
+    }
+
+    private void setLaneSpawner()
+    {
+        Spawner[] spawners = FindObjectsOfType<Spawner>();
+        foreach (Spawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool isAttackerInLine()
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire(float damage)
     {
