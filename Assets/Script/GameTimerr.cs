4c79c92bec9198f5da4632bead99c57114dfbd2c
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-14 21:41:55 +0700

    make timer level with slide and animation

---
+++ Assets/Script/GameTimerr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameTimerr : MonoBehaviour
+{
+    [SerializeField] float LevelTime = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / LevelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= LevelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Finished");
+        }
+    }
+}
