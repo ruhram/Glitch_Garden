f8585b7ee2e71d8dbe109bb630082052650b9088
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-14 19:32:19 +0700

    make attacker attack

--- Assets/Script/Attacker.cs
+++ Assets/Script/Attacker.cs
@@ -6,7 +6,7 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)]
     float currentSpeed = 1f;
-
+    GameObject curremtTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +22,11 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        curremtTarget = target;
     }
 }
