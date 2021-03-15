5ad6d2f2506f3f8680ed8b26ddd8c12b24371d80
ruhram <rullyrgandadiwirya@gmail.com>
2021-03-15 23:33:31 +0700

    set and save music on PlayerPrefs

---
+++ Assets/Script/LevelControlller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControlller : MonoBehaviour
+{
+    [SerializeField] GameObject winLabel;
+    [SerializeField] GameObject loseLabel;
+    [SerializeField] float waitToLoad = 4f;
+    int numberOfAttacker = 0;
+    bool timerFinished = false;
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+        loseLabel.SetActive(false);
+    }
+    public void AttackerSpawned()
+    {
+        numberOfAttacker++;
+    }
+
+    public void AttackerKilled()
+    {
+        numberOfAttacker--;
+        if(numberOfAttacker <= 0 && timerFinished)
+        {
+            StartCoroutine(HandelWinCondition());
+        }
+    }
+    
+    IEnumerator HandelWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
+    public void HandleLoseCondition()
+    {
+        loseLabel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void levelTimerFinished()
+    {
+        timerFinished = true;
+        StopSpawners();
+    }
+
+    private void StopSpawners()
+    {
+        Spawner[] spawners = FindObjectsOfType<Spawner>();
+        foreach(Spawner spawner in spawners)
+        {
+            spawner.stopSpawning();
+        }
+    }
+}
